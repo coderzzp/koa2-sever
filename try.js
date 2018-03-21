5b5e1ffe4539a6d191bf9d1a54b17b5e401d848e
zzp <zzp@zzpdeMacBook-Pro.local>
2018-03-21 11:28:47 +0800

    fix

--- try.js
+++ try.js
@@ -7,7 +7,8 @@ app.use(bodyParser({multipart: true}));
 app.use(async ctx => {
   // the parsed body will store in ctx.request.body 
   // if nothing was parsed, body will be an empty object {} 
-  console.log(ctx.request.body.fields)
-  ctx.body = ctx.request.body.fields;
+  
+  ctx.body = '来自zzp的力量'
 });
-app.listen(3000)
+app.listen(3003)
+console.log('server is running at 3003')
